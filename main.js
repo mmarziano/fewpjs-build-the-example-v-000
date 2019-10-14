8218419923a71bb54dcffe7be860b38c9ff4de2b
Monica Marziano <mmarziano1@gmail.com>
2019-10-14 11:49:46 +0000

    Automatically backed up by Learn

--- main.js
+++ main.js
@@ -3,7 +3,32 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+window.addEventListener('load', function(){
+  createListeners();
+});
 
+function createListeners() {
+  let posts = document.getElementsByTagName('article');
+  for (let i = 0; i < posts.length; i++) {
+        let id = posts[i].id;
+        let glyph = document.getElementById(`${id}`).querySelector('li.like')
+        glyph.addEventListener('click', function() {
+          let heart = glyph.querySelector('.like-glyph');
+          mimicServerCall()
+          .then(function(response) {
+            if(response == "Pretend remote server notified of action!") {
+            heart.innerText = FULL_HEART;
+            heart.className = 'activated-heart'
+          } else {
+            .catch(() => {
+                document.querySelector('#modal').className('hidden').remove
+            })
+          };
+          });
+          
+        });
+  }
+}
 
 
 
